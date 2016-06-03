f934a68cd25ed42668d2e4e4cf1feeea42eb1807
Zebulon McCorkle <zebmccorkle@gmail.com>
2016-06-03 09:57:50 -0500

    Add javadoc to CommandUtil

--- src/main/java/me/zebmccorkle/ircbot/CommandUtil.java
+++ src/main/java/me/zebmccorkle/ircbot/CommandUtil.java
@@ -1,10 +1,21 @@
 package me.zebmccorkle.ircbot;
 
 public class CommandUtil {
+    /**
+     * Get {@link Argument}s of a command
+     * @param command Command to get arguments of
+     * @return Array of {@link Argument} annotations present on {@code command.getClass()}
+     */
     public static Argument[] getArguments(ICommand command) {
         return command.getClass().getAnnotationsByType(Argument.class);
     }
 
+    /**
+     * Check if {@code argc} is enough arguments to execute {@code command}
+     * @param command Command to check argument count of
+     * @param argc Amount of arguments given
+     * @return Whether {@code argc} is enough arguments to execute {@code command}
+     */
     public static boolean enoughArguments(ICommand command, int argc) {
         Argument[] arguments = getArguments(command);
         int lastRequiredIndex = 0;
@@ -15,6 +26,11 @@ public class CommandUtil {
         return (argc - 1) >= lastRequiredIndex;
     }
 
+    /**
+     * Get the amount of required arguments of a command
+     * @param command Command to check required argc of
+     * @return Required argc of command
+     */
     public static int requiredArguments(ICommand command) {
         Argument[] arguments = getArguments(command);
         int i = 0;
@@ -25,6 +41,11 @@ public class CommandUtil {
         return i;
     }
 
+    /**
+     * Get usage string of a command
+     * @param command Command to get usage string of
+     * @return Usage string of command
+     */
     public static String usage(ICommand command) {
         try {
             Argument[] arguments = command.getClass().getAnnotationsByType(Argument.class);
@@ -43,10 +64,20 @@ public class CommandUtil {
         }
     }
 
+    /**
+     * Get the name of a command
+     * @param command Command to get name of
+     * @return Name of command
+     */
     public static String name(ICommand command) {
         return command.getClass().getAnnotation(Name.class).value();
     }
 
+    /**
+     * Get the description of a command
+     * @param command Command to get description of
+     * @return Description of command
+     */
     public static String description(ICommand command) {
         return command.getClass().getAnnotation(Description.class).value();
     }
